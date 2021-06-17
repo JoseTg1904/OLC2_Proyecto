0077699fb1b847ae33648db25112648ecfdbdda8
Diego Berrios <201503609@ingenieria.usac.edu.gt>
2021-06-17 10:55:50 -0600

    ADD: errores analizador XML desc

--- src/analizadores/AST/SalidaGramatica.ts
+++ src/analizadores/AST/SalidaGramatica.ts
@@ -3,10 +3,12 @@ export class SalidaGramatica {
     public objetos: any;
     public reporteBNF: any;
     public reporteBNF2: any;
+    public reportError: any;
 
-    constructor(objetos: any, reporte: any, reporte2: any) {
+    constructor(objetos: any, reporte: any, reporte2: any, reporteE = null) {
         this.objetos = objetos;
         this.reporteBNF = reporte;
         this.reporteBNF2 = reporte2;
+        this.reportError = reporteE;
     }
 }
